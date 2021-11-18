3b109a195f59ab0e22f60db70f9f330bc481696f
nicholas12392 <87773847+nicholas12392@users.noreply.github.com>
2021-11-18 21:18:30 +0100

    Update functions.py

--- nanoscipy/functions.py
+++ nanoscipy/functions.py
@@ -9,7 +9,9 @@ def plot_grid(nr=0,r=1,s=1,share=0,set_dpi=300):
     global figure_global_output
     global ax_global_output
     global figure_number_global_output
+    global share_axis_bool_output
     figure_number_global_output = nr
+    share_axis_bool_output = share
     
     if r == 1 and s == 1:
         figure_global_output, _ax_global_output = plt.subplots(num=nr, dpi=set_dpi)
@@ -17,11 +19,11 @@ def plot_grid(nr=0,r=1,s=1,share=0,set_dpi=300):
     if r > 1 or s > 1:
         if share == 'x' or share == 1:
             figure_global_output, ax_global_output = plt.subplots(r,s,num=nr,sharex=True, dpi=set_dpi)
-        if share == 'y' or share == 2:
+        elif share == 'y' or share == 2:
             figure_global_output, ax_global_output = plt.subplots(r,s,num=nr,sharey=True, dpi=set_dpi)
-        if share == 'xy' or share == 'yx' or share == 'both' or share == 3:
+        elif share == 'xy' or share == 'yx' or share == 'both' or share == 3:
             figure_global_output, ax_global_output = plt.subplots(r,s,num=nr,sharex=True,sharey=True, dpi=set_dpi)
-        if share == 'no' or share == 0:
+        elif share == 'no' or share == 0:
             figure_global_output, ax_global_output = plt.subplots(r,s,num=nr,sharex=False,sharey=False, dpi=set_dpi)
     
     global boundary_ax_global_fix
@@ -29,8 +31,8 @@ def plot_grid(nr=0,r=1,s=1,share=0,set_dpi=300):
 
 def plot_data(p=0,xs=[],ys=[],ttl=None,dlab=[],xlab=None,
                   ylab=None,ms=[],lw=[],ls=[],dcol=[],
-                  plt_type=0,beta=0,tight=True,mark=[],v_ax=[True,'black','dashed',0.5],
-                  h_ax=[True,'black','dashed',0.5],no_ticks=False):
+                  plt_type=0,tight=True,mark=[],v_ax=[True,'black','dashed',0.5],
+                  h_ax=[True,'black','dashed',0.5],no_ticks=False,share_ttl=False):
     if len(ax_global_output) != boundary_ax_global_fix:
         axs = ax_global_output.flatten()
     else:
@@ -52,26 +54,42 @@ def plot_data(p=0,xs=[],ys=[],ttl=None,dlab=[],xlab=None,
         dcol = np.repeat('black',datas)
     if not ls:
         ls = np.repeat('solid',datas)
-    axs[p].set_title(ttl) 
+    
+    # set title according to share_ttl
+    if share_ttl == False:
+        axs[p].set_title(ttl) 
+    elif share_ttl == True:
+        figure_global_output.suptitle(ttl)
+        
     ds = range(datas) 
     if plt_type == 0 or plt_type == 'plot': 
         [axs[p].plot(xs[n],ys[n],c=dcol[n],label=dlab[n],linewidth=lw[n],markersize=ms[n],
                      marker=mark[n],linestyle=ls[n]) for n in ds]  
-    if plt_type == 1 or plt_type == 'scatter' or plt_type == 3 or plt_type == 'fit':
+    if plt_type == 1 or plt_type == 'scatter':
         [axs[p].scatter(xs[n],ys[n],c=dcol[n],label=dlab[n],s=ms[n]) for n in ds]  
     if plt_type == 2 or plt_type == 'qqplot':
         [qqplot(data=xs[n],line='r',ax=axs[p],marker=mark[n],color=dcol[n],label=dlab[n]) for n in ds]
         axs[p+1].boxplot([xs[n] for n in ds],labels=[dlab[n] for n in ds]) 
-    if beta == 1:
-        axs[p+beta].set_xlabel(xlab)
+    
+    # fix labels according to share_axis_bool_output
+    if share_axis_bool_output == 'x' or share_axis_bool_output == 1:
+        axs[-1].set_xlabel(xlab)
         axs[p].set_ylabel(ylab)
-        axs[p+beta].set_ylabel(ylab)
-    if beta == 0: 
+    elif share_axis_bool_output == 'y' or share_axis_bool_output == 2: 
         axs[p].set_xlabel(xlab)
-        axs[p].set_ylabel(ylab)
-    [axs[p+i].legend() for i in range(0,beta+1)]
+        axs[0].set_ylabel(ylab)
+    elif share_axis_bool_output == 'xy' or share_axis_bool_output == 'yx' or share_axis_bool_output == 'both' or share_axis_bool_output == 3:
+        axs[-1].set_xlabel(xlab)
+        axs[0].set_ylabel(ylab) 
+    elif share_axis_bool_output == 'no' or share_axis_bool_output == 0:
+        axs[p].set_xlabel(xlab)
+        axs[p].set_ylabel(ylab) 
+    
+    # set fitted layout according to tight
     if tight == True:
         plt.tight_layout()
+        
+    # set axis tics according to no_ticks
     if no_ticks == True:
         axs[p].set_yticks([])
         axs[p].set_xticks([])
@@ -83,6 +101,9 @@ def plot_data(p=0,xs=[],ys=[],ttl=None,dlab=[],xlab=None,
         v_ax = [False]
     elif v_ax[0] == True:
         plt.axvline(x=0,ymin=0,ymax=1,color=v_ax[1],linestyle=v_ax[2],linewidth=1,alpha=v_ax[3]) 
+    
+    # set legends
+    axs[p].legend() 
         
 def file_select(path=None,set_cols=[0,1],cut_first_row=True): 
     if path == None: 
@@ -139,5 +160,5 @@ def data_extrema(function,pos_index=False):
         return [min_val,max_val]
     elif pos_index == True:
         index_raw = [np.where(data[:,0] == min_val[0]),np.where(data[:,0] == max_val[0])] # index extremas
-        index_list = [index_raw[0][0],index_raw[1][0]]
+        index_list = [[index_raw[0][0][0]],[index_raw[1][0][0]]]
         return [min_val,max_val], index_list
